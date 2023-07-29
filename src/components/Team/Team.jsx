79029ff6b1197a88e262ccc5c2b37118d99ebb77
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-07-29 16:38:24 -0300

    Conditional Teams

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -4,16 +4,23 @@ import { Card } from "../Card/Card";
 
 export const Team = ({ name, primaryColor, secondColor, collaborators }) => {
   return (
-    <section className="time" style={{ backgroundColor: secondColor }}>
-      <h3 style={{ borderColor: primaryColor }}>{name}</h3>
+    collaborators.length > 0 && (
+      <section className="time" style={{ backgroundColor: secondColor }}>
+        <h3 style={{ borderColor: primaryColor }}>{name}</h3>
 
-      <div className="colaboradores">
-        {collaborators.map((e) => {
-          return (
-            <Card name={e.name} office={e.office} img={e.image} key={e.name} />
-          );
-        })}
-      </div>
-    </section>
+        <div className="colaboradores">
+          {collaborators.map((e) => {
+            return (
+              <Card
+                name={e.name}
+                office={e.office}
+                img={e.image}
+                key={e.name}
+              />
+            );
+          })}
+        </div>
+      </section>
+    )
   );
 };
